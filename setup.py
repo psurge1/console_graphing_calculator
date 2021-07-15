8d8062ba81b6096856671afb1793b859444a349d
psurge1 <suraj.sswamy@gmail.com>
2021-07-15 09:50:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-  name='',
-  version='',
-  description='',
-  py_modules=[''],
-  package_dir={'':''},
+  name='linear_math',
+  version='0.0.1',
+  description='Manipulate linear equations and plot them in the IDE',
+  py_modules=['linear_math'],
+  package_dir={'':'src'},
 )
