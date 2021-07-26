6913ea25609afd330ba660e5ca23937abfaecf01
psurge1 <suraj.sswamy@gmail.com>
2021-07-26 14:47:23 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-  name='graphing_calculator',
-  version='0.0.1',
-  description='Manipulate linear equations and plot them in the IDE',
-  py_modules=['graphing_calculator'],
-  package_dir={'':'src'},
-)
