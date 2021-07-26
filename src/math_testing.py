6523b92aa3e29a8a89636160fbc5e98b7116371c
psurge1 <suraj.sswamy@gmail.com>
2021-07-25 21:23:30 -0500

    Update math_testing.py

--- src/math_testing.py
+++ src/math_testing.py
@@ -1,5 +1,16 @@
+'''
+Python file of commands
+'''
+
 from graphing_calculator import linear
 
-a = linear('y=3x+2')
-print(a.equation())
-print(a.equation(form='standard'))
+
+a = linear('y=2x+0')
+#print(a.equation())
+#print(a.equation(form='standard'))
+#print(a.console_plot())
+#print(a.properties())
+#print(a.x_factor)
+#print(a.y_factor)
+#print(a.b)
+#print(a.m)
