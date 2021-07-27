753ad41d012c231e9cdc6d56b34a818e541c9a25
psurge1 <suraj.sswamy@gmail.com>
2021-07-26 19:36:44 -0500

    Update math_testing.py

--- math_testing.py
+++ math_testing.py
@@ -3,7 +3,7 @@ Python file of sample commands
 Check the README for more info
 '''
 
-from graphing_calculator import linear
+from console_graphing_calculator import linear
 
 
 a = linear('y=2x+0')
